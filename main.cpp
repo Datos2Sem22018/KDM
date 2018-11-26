6af03017139bc7683365ddb5a7bce0af9b025ea7
Karla Rivera <karla.rivera24@gmail.com>
2018-11-26 06:51:08 -0600

    client Json por probar con Proyecto3

--- main.cpp
+++ main.cpp
@@ -7,14 +7,11 @@ int run(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
     return a.exec();
 }
 
 int main(int argc, char *argv[])
 {
-
     run(argc, argv);
-
     return 0;
 }
